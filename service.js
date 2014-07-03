ea43440083423fb4e931ae2900a3735ab0f35121
Theo Julienne <theo@ninjablocks.com>
2014-07-03 18:13:07 +1000

    Initial import of userspace RPC agent.

---
+++ service.js
@@ -0,0 +1,19 @@
+process.title = 'sphere-cloud-rpc';
+
+if (process.env.BUGSNAG_KEY) {
+  var bugsnag = require("bugsnag");
+  bugsnag.register(process.env.BUGSNAG_KEY, { releaseStage: process.env.USVC_CONFIG_ENV || 'development' });
+}
+
+var usvc = require('usvc');
+
+var service = usvc.microService({
+  // bus connections
+  amqp: usvc.facets.msg.amqp(),
+
+  // userspace message routing (MQTT -> service facets)
+  userspaceRPCHook: require('./lib/userspace_rpc')([ 'userNotificationRPC' ]),
+
+  // userspace RPC services
+  userNotificationRPC: require('./lib/user_rpc/userNotificationRPC'),
+}).run();
